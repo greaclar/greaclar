1669da04450da4f4fa3c454f174ee68651e810a9
ljq <greaclar@163.com>
2023-06-10 12:28:36 +0800

    [fix]:移动端无法自动触发点击事件

--- js/meau.js
+++ js/meau.js
@@ -2,28 +2,31 @@ window.addEventListener('load', function windowFirstLoad() {
     const meau = document.querySelector('.meau');
     const itemHeads = meau.querySelectorAll('.item-head');
     const itemBodys = meau.querySelectorAll('.item-body');
+
+    let eventType;
+    let EvenIns;
+    if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
+        eventType = 'touchend';
+    } else {
+        eventType = 'click';
+    }
+    EvenIns = new Event(eventType);
     // 为每个选项头添加点击事件，相应的选项体会展开
     for (let i = 0; i < itemHeads.length; i++) {
-        let eventType;
-        if ((navigator.userAgent.match(/(phone|pad|pod|iPhone|iPod|ios|iPad|Android|Mobile|BlackBerry|IEMobile|MQQBrowser|JUC|Fennec|wOSBrowser|BrowserNG|WebOS|Symbian|Windows Phone)/i))) {
-            eventType = 'touchend';
-        } else {
-            eventType = 'click';
-        }
         itemHeads[i].addEventListener(eventType, function () {
 
             // 如果点击的是最后一个收起选项，则不打开任何选项体
             if (i === itemHeads.length - 1 || itemBodys[i].getAttribute('flod') === '1') {
                 flodAll(itemBodys)
                 return;
-            }else{
+            } else {
                 flodAll(itemBodys)
                 unflod(itemBodys[i])
             }
         })
     }
 
-    itemHeads[0].click();
+    itemHeads[0].dispatchEvent(EvenIns);
     window.removeEventListener('load', windowFirstLoad)
 
     // 把传入的所有元素调用一次flod函数
